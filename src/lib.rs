c8d796a6aa26a97d57b3774f19fa1b7c45ee3971
Rad Frog <radfrogradfrog@gmail.com>
2025-05-04 15:44:01 -0400

    Single-register read access

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
 #![no_std]
+#![forbid(unsafe_code)]
+#![warn(clippy::cargo)]
+
+mod ral;
+mod registers;
 
 use embedded_hal::spi::SpiDevice;
+use ral::read_register;
+use registers::{PARTNUMBER, PARTVERSION};
 
-struct CC1200<Spi: SpiDevice> {
+pub struct CC1200<Spi: SpiDevice> {
     spi: Spi,
 }
 
 impl<Spi: SpiDevice> CC1200<Spi> {
-    fn new(spi: Spi) -> Self {
-        Self {
-            spi,
-        }
+    pub fn new(spi: Spi) -> Self {
+        Self { spi }
+    }
+
+    pub fn part_number(&mut self) -> Result<u8, Spi::Error> {
+        Ok(read_register::<Spi, PARTNUMBER>(&mut self.spi)?.into())
+    }
+
+    pub fn part_version(&mut self) -> Result<u8, Spi::Error> {
+        Ok(read_register::<Spi, PARTVERSION>(&mut self.spi)?.into())
     }
 }
---
+++ src/ral.rs
@@ -0,0 +1,37 @@
+use bitfield::bitfield;
+use embedded_hal::spi::{Operation, SpiDevice};
+
+use crate::registers::{Register, RegisterAddress};
+
+bitfield! {
+    struct Header(u8);
+
+    _, set_write_bit: 7;
+    _, set_burst_bit: 6;
+    _, set_address: 5, 0;
+}
+
+pub fn read_register<Spi: SpiDevice, R: Register>(spi: &mut Spi) -> Result<R, Spi::Error> {
+    let mut buf = [0_u8];
+    match R::ADDRESS {
+        RegisterAddress::Config(addr) => {
+            let mut header = Header(0x00);
+            header.set_address(addr & 0x3F);
+            header.set_burst_bit(false);
+            header.set_write_bit(false);
+            spi.transaction(&mut [Operation::Write(&[header.0]), Operation::Read(&mut buf)])?;
+        }
+        RegisterAddress::Extended(addr) => {
+            let mut header = Header(0x00);
+            header.set_address(0x2f);
+            header.set_burst_bit(false);
+            header.set_write_bit(false);
+            spi.transaction(&mut [
+                Operation::Write(&[header.0]),
+                Operation::Write(&[addr]),
+                Operation::Read(&mut buf),
+            ])?;
+        }
+    };
+    Ok::<R, Spi::Error>(buf[0].into())
+}
